041ae8cafd591f52923910f4320393eb3c573201
DiegoAlign06 <114187502+DiegoAlign06@users.noreply.github.com>
2022-10-05 13:37:51 -0600

    Add Handling Forms to the Main Project
    Main functionality finished

--- FinalProject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ FinalProject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from '../recipe.service';
@@ -9,15 +10,21 @@ import { RecipeService } from '../recipe.service';
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
-export class RecipeListComponent implements OnInit {
+export class RecipeListComponent implements OnInit,OnDestroy {
 
   recipes: Recipe[];
-
+  subscription : Subscription;
   constructor(private recipeService:RecipeService,
               private router: Router,
               private route: ActivatedRoute) { }
 
-  ngOnInit() {
+  ngOnInit() {//aca ahora podemos escuchar el evento emitido en recipe.service con el recipesChanged
+    this.subscription = this.recipeService.recipesChanged.
+    subscribe(
+      (recipes:Recipe[]) =>{
+        this.recipes = recipes;
+      }
+    );
     this.recipes = this.recipeService.getRecipes();
   }
 
@@ -25,5 +32,7 @@ export class RecipeListComponent implements OnInit {
   onNewRecipe(){
     this.router.navigate(['new'], {relativeTo: this.route});
   }
-  
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
+  }
 }
--- FinalProject/src/app/shopping-list/shopping-list.service.ts
+++ FinalProject/src/app/shopping-list/shopping-list.service.ts
@@ -1,9 +1,10 @@
-import { EventEmitter } from "@angular/core";
+
 import { Subject } from "rxjs";
 import { Ingredient } from "../shared/ingredient.model";
 
 export class ShoppingListService{
-    ingredientsChanged = new Subject<Ingredient[]>;
+    ingredientsChanged = new Subject<Ingredient[]>;//emite el ingrediente[]
+    startedEditing = new Subject<number>();
     private ingredients: Ingredient[] = [
         new Ingredient('Apples', 5),
         new Ingredient('Tomatoes', 10),
@@ -11,12 +12,12 @@ export class ShoppingListService{
       getIngredients(){
         return this.ingredients.slice();
       }
+      getIngredient(index:number){
+        return this.ingredients[index];
+      }
       addIngredient(Ingredient:Ingredient){
         this.ingredients.push(Ingredient );
         this.ingredientsChanged.next(this.ingredients.slice());
-
-        
-
       }
       addIngredients(ingredients:Ingredient[]){
         // for (let ingredient of this.ingredients){
@@ -25,4 +26,12 @@ export class ShoppingListService{
          this.ingredients.push(...ingredients);
          this.ingredientsChanged.next(this.ingredients.slice());
       }
+    updateIngredient(index:number, newIngredient:Ingredient){
+      this.ingredients[index] = newIngredient;
+      this.ingredientsChanged.next(this.ingredients.slice());
+    }
+    deleteIngredient(index:number){
+      this.ingredients.splice(index,1);
+      this.ingredientsChanged.next(this.ingredients.slice());
+    }
 }
